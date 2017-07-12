a5c688339b22694de40af5869200a3e7002f932d
Calvin Giles <calvin@lyst.com>
2017-07-12 16:19:45 +0100

    Add peek methods and force bury. Resolves #9 and #10.

--- src/shovel/exceptions.py
+++ src/shovel/exceptions.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from textwrap import dedent
 
 
 class VersionError(ValueError):
@@ -19,3 +20,17 @@ class DatasetExists(ValueError):
 
 class FileExists(ValueError):
     pass
+
+
+class OutOfSync(ValueError):
+    def __init__(self, local_files, remote_files):
+        only_local = sorted(set(local_files) - set(remote_files))
+        only_remote = sorted(set(remote_files) - set(local_files))
+        local_and_remote = sorted(set(remote_files) & set(local_files))
+        msg = dedent("""
+            Local and remote not in sync.
+            Only local: {l}
+            Only remote: {r}
+            In both: {b}
+            """).strip().format(l=only_local, r=only_remote, b=local_and_remote)
+        super().__init__(msg)
